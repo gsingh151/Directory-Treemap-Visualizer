cec2ca1698148e0f2b487154931a598f136fa77e
gsingh151 <45342803+gsingh151@users.noreply.github.com>
2020-06-27 22:59:47 -0400

    Update papers.py

--- papers.py
+++ papers.py
@@ -1,63 +1,3 @@
-"""Assignment 2: Modelling CS Education research paper data
-
-=== CSC148 Winter 2019 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2019 Bogdan Simion, David Liu, Diane Horton, Jacqueline Smith
-
-=== Module Description ===
-This module contains a new class, PaperTree, which is used to model data on
-publications in a particular area of Computer Science Education research.
-This data is adapted from a dataset presented at SIGCSE 2019.
-You can find the full dataset here: https://www.brettbecker.com/sigcse2019/
-
-Although this data is very different from filesystem data, it is still
-hierarchical. This means we are able to model it using a TMTree subclass,
-and we can then run it through our treemap visualisation tool to get a nice
-interactive graphical representation of this data.
-
-TODO: (Task 6) Complete the steps below
-Recommended steps:
-1. Start by reviewing the provided dataset in cs1_papers.csv. You can assume
-   that any data used to generate this tree has this format,
-   i.e., a csv file with the same columns (same column names, same order).
-   The categories are all in one column, separated by colons (':').
-   However, you should not make assumptions about what the categories are, how
-   many categories there are, the maximum number of categories a paper can have,
-   or the number of lines in the file.
-
-2. Read through all the docstrings in this file once. There is a lot to take in,
-   so don't feel like you need to understand it all the first time.
-   Draw some pictures!
-   We have provided the headers of the initializer as well as of some helper
-   functions we suggest you implement. Note that we will not test any
-   private top-level functions, so you can choose not to implement these
-   functions, and you can add others if you want to for your solution.
-   For this task, we will be testing that you are building the correct tree,
-   not that you are doing it in a particular way. We will access your class
-   in the same way as in the client code in the visualizer.
-
-3. Plan out what you'll need to do to implement the PaperTree initializer.
-   In particular, think about how to use the boolean parameters to do different
-   things in setting up the tree. You may also find it helpful to review the
-   Python documentation about the csv module, which you are permitted and
-   encouraged to use. You should have a good plan, including what your subtasks
-   are, before you begin writing any code.
-
-4. Write the code for the PaperTree initializer and any helper functions you
-   want to use in your design. You should not make any changes to the public
-   interface of this module, or of the PaperTree class, but you can add private
-   attributes and helpers as needed.
-
-5. Tidy and test your code, and try it with the visualizer client code. Make
-   sure you have documented any new private attributes, and that PyTA passes
-   on your code.
-"""
 import csv
 from typing import List, Dict
 from tm_trees import TMTree
@@ -233,9 +173,3 @@ class PaperTree(TMTree):
 
 
 if __name__ == '__main__':
-    import python_ta
-    python_ta.check_all(config={
-        'allowed-import-modules': ['python_ta', 'typing', 'csv', 'tm_trees'],
-        'allowed-io': ['_load_papers_to_dict'],
-        'max-args': 8
-    })
